d99214466ddcf8726aee3ac0373c05fad0ffe3af
Filipe Correia <1150524@isep.ipp.pt>
2017-03-24 11:06:29 +0000

    Exercicio 13 Ficha 2

---
+++ entrega1/ficha2/ex11/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void){
+
+
+
+	return 0;
+}
---
+++ entrega1/ficha2/ex13/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void){
+
+	const int BUFFER_SIZE = 80;
+
+	char buffer[BUFFER_SIZE];
+
+	pid_t pid;
+
+	int fd[2], n;
+
+	pipe(fd);
+
+	pid = fork();
+
+	if( pid < 0){
+
+		perror("Falha no fork!");
+
+	}
+
+	if( pid > 0){
+
+		close(fd[1]);
+
+		while((n = read(fd[0], buffer, 80)) > 0){
+
+			buffer[n-1] = 0;
+			printf("%s\n", buffer);
+
+		}
+
+		close(fd[0]);
+		wait(NULL);
+
+	}
+	if( pid == 0){
+
+		close(fd[0]);
+
+		dup2(fd[1],1);
+
+		close(fd[1]);
+
+		execlp("sort", "sort", "fx.txt", (char*) NULL);
+
+		perror("Exec error");
+
+		exit(1);
+
+	}
+
+	return 0;
+
+}
