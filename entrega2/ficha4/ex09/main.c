d7d184d24e09820af11bf23a33e3fb71725061fa
Filipe Correia <1150524@isep.ipp.pt>
2017-04-29 00:21:39 +0000

    Correção do exercicio 9 da ficha 4

--- entrega2/ficha4/ex09/main.c
+++ entrega2/ficha4/ex09/main.c
@@ -12,10 +12,11 @@
 #define CHILD_NUM 2
 #define VALUES 30
 #define SHM_NAME "shm_ex09"
+#define BUFFER_SIZE 10
 
 typedef struct{
 	int array [10];
-	int position;
+	int pos;
 	int end;
 }buffer;
 
@@ -23,39 +24,46 @@ int main(void) {
 
 	int i;
 	pid_t pid;
-	sem_t *producer;
-	sem_t *consumer;
+	sem_t *full;
+	sem_t *mutex;
+	sem_t *empty;
 	int data_size = sizeof(buffer);
-	int fd = shm_open(SHM_NAME, O_CREAT|O_EXCL| O_RDWR, S_IRUSR | S_IWUSR);
 
-	if((producer = sem_open("producer", O_CREAT|O_EXCL, 0644, 1)) == SEM_FAILED) {
+	if((full = sem_open("full", O_CREAT|O_EXCL, 0644, 0)) == SEM_FAILED) {
 		perror("Error at first semaphore");
-		exit(1);
+		exit(-1);
 	}
 
-	if((consumer = sem_open("consumer", O_CREAT|O_EXCL, 0644, 0)) == SEM_FAILED) {
+	if((mutex = sem_open("mutex", O_CREAT|O_EXCL, 0644,1)) == SEM_FAILED)  {
 		perror("Error at second semaphore");
-		exit(2);
+		exit(-1);
 	}
+
+	if((empty = sem_open("empty", O_CREAT|O_EXCL, 0644, BUFFER_SIZE)) == SEM_FAILED) {
+		perror("Error at third semaphore");
+		exit(-1);
+	}
+
+	int fd = shm_open(SHM_NAME, O_CREAT|O_EXCL| O_RDWR, S_IRUSR | S_IWUSR);
 
 	if(fd<0) {
 		perror("Error creating file");
-		exit(3);
+		exit(-1);
 	}
 
 	if(ftruncate(fd, data_size) < 0) {
 		perror("Error defining size");
-		exit(4);
+		exit(-1);
 	}
 
 	buffer * shared_data = (buffer *) mmap(NULL, data_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 
 	if(shared_data == NULL) {
 		perror("Mapping error");
-		exit(5);
+		exit(-1);
 	}
 
-	shared_data->position = 10;
+	shared_data->pos = 0;
 
 	for(i = 0; i<CHILD_NUM; i++) {
 		pid = fork();
@@ -66,78 +74,86 @@ int main(void) {
 	
 	if(pid<0) {
 		perror("Fork error");
-		exit(6);
+		exit(-1);
 	}
 	
 	if(pid==0) {
-		int incrementer = 0;
-		sem_wait(producer);
-		
-		for(;;) {
-			shared_data->position = shared_data->position - 1;
-			shared_data->array[shared_data->position] = incrementer;
-			if(shared_data->position == 0) {
-				sem_post(consumer);
-				sem_wait(producer);
-			}
-			if(shared_data->end == (VALUES-1)) {
-				break;
-			}
-			incrementer++;
-		}
+		int number = 0;
+		do {
+			sem_wait(empty);
+			sem_wait(mutex);
 
+			shared_data->array[shared_data->pos] = number;
+			shared_data->pos = shared_data->pos + 1;
+			number++;
+
+			sem_post(mutex);
+			sem_post(full);
+
+		} while(shared_data->end != (VALUES-1));
+		
 		exit(1);
 	}
-	
+
 	if(pid>0) {
 
-		sem_wait(consumer);
-		for(i=0; i<VALUES; i++) {
-			printf("Valor: %d\n", shared_data->array[shared_data->position]);
-			shared_data->position = shared_data->position + 1;
+		do {
+			sem_wait(full);
+			sem_wait(mutex);
+
+			printf("%d\n", shared_data->array[shared_data->pos]);
+			shared_data->pos = shared_data->pos - 1;
 			shared_data->end = shared_data->end + 1;
 
-			if(shared_data->position == 10) {
-				sem_post(producer);
-				sem_wait(consumer);
-			}
+			sem_post(mutex);
+			sem_post(empty);
+
+		} while(shared_data->end != (VALUES -1));
+
+		if(sem_close(mutex)<0) {
+			perror("Error closing mutual exclusion semaphore");
+			exit(-1);
+		}
+		
+		if(sem_close(full)<0) {
+			perror("Error closing full semaphore");
+			exit(-1);
 		}
 
-		if(sem_close(producer)<0) {
-			perror("Error closing producer");
-			exit(7);
+		if(sem_close(empty)<0) {
+			perror("Error closing empty semaphore");
+			exit(-1);
 		}
 		
-		if(sem_close(consumer)<0) {
-			perror("Error closing consumer");
-			exit(8);
+		if(sem_unlink("empty")<0) {
+			perror("Error unlinking empty semaphore");
+			exit(-1);
 		}
 		
-		if(sem_unlink("producer")<0) {
-			perror("Error unlinking producer");
-			exit(9);
-		}
-		
-		if(sem_unlink("consumer")<0) {
-			perror("Error unlinking consumer");
-			exit(10);
+		if(sem_unlink("full")<0) {
+			perror("Error unlinking full semaphore");
+			exit(-1);
 		}
 
+		if(sem_unlink("mutex")<0) {
+			perror("Error unlinking mutual exclusion semaphore");
+			exit(-1);
+		}
 	}
 	
 	if(munmap(shared_data, data_size)<0) {
 		perror("Unmapping error");
-		exit(11);
+		exit(-1);
 	}
 
 	if(close(fd)<0) {
 		perror("Error closing shared memory");
-		exit(12);
+		exit(-1);
 	}
 
 	if(shm_unlink(SHM_NAME)<0) { 
 		perror("Error closing shared memory");
-		exit(13);
+		exit(-1);
 	}
 
 	return 0;
