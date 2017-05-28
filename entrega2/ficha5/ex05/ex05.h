e3891708e585316e4771657c481a1dbb081939be
Filipe Correia <1150524@isep.ipp.pt>
2017-05-28 03:54:31 +0100

    Exercicio 5 Ficha 5

--- entrega2/ficha5/ex05/ex05.h
+++ entrega2/ficha5/ex05/ex05.h
@@ -1,9 +1,12 @@
 #define DATA_SIZE 1000
 #define CALCULATE_NUM 200
+#define THREAD_NUM 5
+
 
 int resultados[DATA_SIZE];
 int dados[DATA_SIZE];
 int threadVerify;
 pthread_mutex_t  mutex;
+pthread_cond_t cond;
 
 void * thread_func (void *arg);
--- entrega2/ficha5/ex05/main.c
+++ entrega2/ficha5/ex05/main.c
@@ -8,12 +8,12 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <time.h>
 #include "ex05.h"
-
-#define THREAD_NUM 5
 
 int main(void) {
 
+	srand(time(NULL));
 	threadVerify = 0;
 
 	if( (pthread_mutex_init(&mutex, NULL)) != 0) {
@@ -21,11 +21,16 @@ int main(void) {
 		exit(0);
 	}
 
+	if( (pthread_cond_init(&cond, NULL)) != 0) {
+		perror("Error creating mutex lock");
+		exit(0);
+	}
+
 	pthread_t threads[THREAD_NUM];
-	int i; 
+	int i;
 
 	for(i=0; i<DATA_SIZE; i++) {
-		dados[i] = i;
+		dados[i] = rand() %100;
 	}
 
 	for(i=0; i<THREAD_NUM; i++) {
@@ -35,6 +40,11 @@ int main(void) {
 		}
 	}
 
+	for(i=0; i<THREAD_NUM; i++) {
+		pthread_join(threads[i], NULL);
+	}
+
+	pthread_cond_destroy(&cond);
 	pthread_mutex_destroy(&mutex);
 
 	return 0;
